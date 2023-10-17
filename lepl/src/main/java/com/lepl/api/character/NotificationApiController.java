6c81db45ca659a583f8c8b31a52a54cc65ef27aa
yujinchoi20 <dalsu4641@naver.com>
2023-10-17 20:21:11 +0900

    update: NotificationAPI 추가

--- lepl/src/main/java/com/lepl/api/character/NotificationApiController.java
+++ lepl/src/main/java/com/lepl/api/character/NotificationApiController.java
@@ -1,4 +1,61 @@
 package com.lepl.api.character;
 
+import com.lepl.Service.character.NotificationService;
+import com.lepl.Service.member.MemberService;
+import com.lepl.api.argumentresolver.Login;
+import com.lepl.domain.character.Notification;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/notification")
 public class NotificationApiController {
+    private final MemberService memberService;
+    private final NotificationService notificationService;
+
+    /**
+     * 알림 조회 API
+     */
+    @GetMapping("/all")
+    public ResponseEntity<List<FindNotificationDto>> findAllWithCharacter(@Login Long memberId) {
+        Long characterId = memberService.findOne(memberId).getCharacter().getId();
+        List<Notification> notifications = notificationService.findAllWriteCharacter(characterId);
+
+        if(notifications.isEmpty()) {
+            return null;
+        }
+
+        List<FindNotificationDto> result = notifications.stream()
+                .map(o -> new FindNotificationDto(o))
+                .collect(Collectors.toList());
+
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    // DTO
+    @Getter
+    static class FindNotificationDto {
+        private Long id;
+        private String content;
+        private LocalDateTime startTime;
+        private LocalDateTime endTime;
+        public FindNotificationDto(Notification notification) {
+            this.id = notification.getId();
+            this.content = notification.getContent();
+            this.startTime = notification.getStartTime();
+            this.endTime = notification.getEndTime();
+        }
+    }
 }
